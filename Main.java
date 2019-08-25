8286faa999a5cdf12a451716a18fadf487d663bc
tumami27kan <54294206+tumami27kan@users.noreply.github.com>
2019-08-25 23:36:03 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static Box box;
+    public static MainFrame mainFrame;
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+            Main.box = new Box();
+            Main.mainFrame =new MainFrame("To Do");
+            Main.mainFrame.setVisible(true);
+    }
+
+}
